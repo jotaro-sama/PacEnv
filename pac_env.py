0fe4ad48a8b35197504c69fea672c1192e8a0fd9
jotaro-sama <36264038+jotaro-sama@users.noreply.github.com>
2018-12-20 09:53:01 +0100

    fix environment and test

--- pac_env.py
+++ pac_env.py
@@ -1,46 +1,57 @@
 import numpy as np 
 
 class PacActionSpace(object):
-    def __init__(self):
-        self.moves = ["up", "left", "right", "dowm"]
-    def sample():
-        return np.random.choice(a=moves)
+    moves = ["up", "left", "right", "down"]
+    def sample(self):
+        return np.random.choice(a=self.moves)
 
 init_state = [
-            ["W", "W", "W", "W", "W", "W", "W", "W"],
-            ["W", ".", ".", "G", "G", ".", ".", "W"],
-            ["W", ".", "W", "W", "W", "W", ".", "W"],
-            [".", ".", ".", ".", ".", ".", ".", "."],
-            ["W", ".", "W", ".", " ", "W", ".", "W"],
-            ["W", ".", "W", ".", ".", "W", ".", "W"],
-            ["W", ".", ".", ".", "P", ".", ".", "W"],
-            ["W", "W", "W", "W", "W", "W", "W", "W"]
+           ["W", "W", "W", "W", "W", "W", "W", "W"],
+           ["W", ".", ".", "G", "G", ".", ".", "W"],
+           ["W", ".", "W", "W", "W", "W", ".", "W"],
+           [".", ".", ".", ".", ".", ".", ".", "."],
+           ["W", ".", "W", "o", ".", "W", ".", "W"],
+           ["W", ".", "W", ".", ".", "W", ".", "W"],
+           ["W", ".", ".", ".", "P", ".", ".", "W"],
+           ["W", "W", "W", "W", "W", "W", "W", "W"]
         ]
+
+power_duration = 6
+
+def CheckOver(level_matrix):
+    over = True
+    for line in level_matrix:
+        if "." in line:
+            over = False
+    return over
 
 class PacEnv():
     def __init__(self):
-        self.level_state = init_state
+        self.state = init_state
         self.score = 0
         self.game_over = False
         self.action_space = PacActionSpace()
-        self.position = (6, 4)
+        self.position = [6, 4]
+        self.power = False
+        self.power_timeout = 0
+    
     def step(self, a):
         reward = 0.0
         if a not in self.action_space.moves:
             raise ValueError
         else:
-            new_pos = 0, 0
             pos = self.position
+            new_pos = pos
             st = self.state
 
             if a=="up":
-                new_pos = pos[0] - 1, pos[1]
+                new_pos = [pos[0] - 1, pos[1]]
             elif a=="left":
-                new_pos = pos[0], pos[1] -1
+                new_pos = [pos[0], pos[1] -1]
             elif a=="right":
-                new_pos = pos[0], pos[1] + 1
+                new_pos = [pos[0], pos[1] + 1]
             elif a=="down":
-                new_pos = pos[0] + 1, pos[1]
+                new_pos = [pos[0] + 1, pos[1]] 
 
             if (new_pos[0] < 0):
                  new_pos[0] = 7
@@ -52,23 +63,49 @@ class PacEnv():
                 new_pos[1] = 0
 
             if st[new_pos[0]][new_pos[1]] == "G":
-                self.game_over = True
+                if not self.power:
+                    self.game_over = True
+                else:
+                    self.score += 3
+                    self.state[new_pos[0]][new_pos[1]] = "P"
+                    self.state[pos[0]][pos[1]] = " "
+                    self.position = new_pos
+                    self.state = st
             elif st[new_pos[0]][new_pos[1]] == ".":
                 self.score += 1
-                st[new_pos[0]][new_pos[1]] == "P"
-                st[pos[0]][pos[1]] == " "
+                self.state[new_pos[0]][new_pos[1]] = "P"
+                self.state[pos[0]][pos[1]] = " "
                 self.position = new_pos
+                self.state = st
+            elif st[new_pos[0]][new_pos[1]] == "o":
+                self.score += 1
+                self.state[new_pos[0]][new_pos[1]] = "P"
+                self.state[pos[0]][pos[1]] = " "
+                self.position = new_pos
+                self.state = st
+                self.power = True
+                self.power_timeout = power_duration
             elif st[new_pos[0]][new_pos[1]] == " ":
-                st[new_pos[0]][new_pos[1]] == "P"
-                st[pos[0]][pos[1]] == " "
+                self.state[new_pos[0]][new_pos[1]] = "P"
+                self.state[pos[0]][pos[1]] = " "
                 self.position = new_pos
-        ob = (self.level_state, self.score)
-        return ob, reward, self.game_over, {"self.position" : self.position}
+        if self.power:
+            if self.power_timeout == 0:
+                self.power = False
+            else:
+                self.power_timeout -= 1
+
+        self.game_over = CheckOver(self.state)
+
+        ob = (self.state, self.score, self.power)
+        return ob, reward, self.game_over, {"self.position" : self.position, "self.power_timeout" : self.power_timeout}
 
     def reset(self):
-        self.level_state = init_state
+        self.state = init_state
         self.score = 0
         self.game_over = False
         self.action_space = PacActionSpace()
-        self.position = (6, 4)
-        return (self.level_state, self.score)
+        self.position = [6, 4]
+        self.power = False
+        self.power_timeout = 0
+        return (self.state, self.score, self.power)
--- pac_test.py
+++ pac_test.py
@@ -1,4 +1,27 @@
-import importlib
+import pac_env
 
-importlib.import_module("pac-man")
-print(pac-man.PacEnv())
+def printMatrix(mat):
+    for row in mat:
+        for elem in row:
+            print(str(elem), end=" ")
+        print("")
+
+env = pac_env.PacEnv()
+(screen, score, power) = env.reset()
+done = False
+info = ""
+
+print("Score: " + str(score))
+printMatrix(screen)
+
+for i in range(100):
+        action = env.action_space.sample()
+        print(action)
+        (screen, score, power), _, done, info = env.step("left")
+        printMatrix(screen)
+        print("Score: " + str(score) + ", Power :" + str(power))
+        print(str(info["self.position"]) + ", pow_timeout: " + str(info["self.power_timeout"]))
+        if done:
+                print("Game Over! \n" + "Score: " + str(score))
+                break
+
